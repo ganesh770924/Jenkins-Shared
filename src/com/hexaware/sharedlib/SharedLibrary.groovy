ad78bae48b37b64a65a6f9dcaf7613092c0ab60a
ganesh770924 <80471973+ganesh770924@users.noreply.github.com>
2021-12-23 20:45:52 +0530

    Create SharedLibrary.groovy

---
+++ src/com/hexaware/sharedlib/SharedLibrary.groovy
@@ -0,0 +1,12 @@
+import com.hexaware.sharedlib
+public class SharedLibrary{
+  def steps
+  
+  public SharedLibrary(steps){
+    this.steps=steps
+  }
+  
+  public void startbuild(){
+     steps.echo "Hello World!"
+    }
+}
