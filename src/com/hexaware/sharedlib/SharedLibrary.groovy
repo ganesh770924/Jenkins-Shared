79b98b6af96d2b0a66434cc3fb7fa623b06768d6
ganesh770924 <80471973+ganesh770924@users.noreply.github.com>
2022-02-07 18:39:14 +0530

    Update SharedLibrary.groovy

--- src/com/hexaware/sharedlib/SharedLibrary.groovy
+++ src/com/hexaware/sharedlib/SharedLibrary.groovy
@@ -2,11 +2,12 @@ package com.hexaware.sharedlib
 public class SharedLibrary{
   def steps
   
-  public SharedLibrary(steps){
+  public SharedLibrary(steps,name){
     this.steps=steps
+    this.name=name
   }
   
   public void startbuild(){
-     steps.echo "Hello World!"
+    steps.echo "Hello World ,${this.name}"
     }
 }
